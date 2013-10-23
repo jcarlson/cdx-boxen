e0dba4ebd5b90c48d34bded5d84454d16017dc12
Jarrod Carlson <jarrod.carlson@gmail.com>
2013-10-23 19:45:54 -0400

    Tweak the default out-of-box configuration

--- manifests/site.pp
+++ manifests/site.pp
@@ -53,15 +53,10 @@ Homebrew::Formula <| |> -> Package <| |>
 
 node default {
   # core modules, needed for most things
-  include dnsmasq
+  # include dnsmasq
   include git
   include hub
-  include nginx
-
-  # fail if FDE is not enabled
-  if $::root_encrypted == 'no' {
-    fail('Please enable full disk encryption and try again')
-  }
+  # include nginx
 
   # node versions
   include nodejs::v0_4
@@ -70,10 +65,10 @@ node default {
   include nodejs::v0_10
 
   # default ruby versions
-  include ruby::1_8_7
-  include ruby::1_9_2
-  include ruby::1_9_3
-  include ruby::2_0_0
+  # include ruby::1_8_7
+  # include ruby::1_9_2
+  include ruby::1_9_3_p448
+  # include ruby::2_0_0
 
   # common, useful packages
   package {
